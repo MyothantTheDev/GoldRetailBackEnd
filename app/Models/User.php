3fd9310682683c11c48b14ad73cefe1d370ae634
Myo Thant <jamesrobinson636478@gmail.com>
2023-12-26 22:26:57 +0700

    Create User Collection

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,21 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Jenssegers\Mongodb\Eloquent\Model;
 
-class User extends Authenticatable
+
+class User extends Model implements AuthenticatableContract
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Authenticatable, Notifiable;
+
+    protected $connection = "mongodb";
+    protected $collection = "users";
 
     /**
      * The attributes that are mass assignable.
