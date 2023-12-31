9f2496b13a85fdd3dddacaec89caad39dc7af426
Myo Thant <jamesrobinson636478@gmail.com>
2024-01-01 02:00:59 +0700

    Add User Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -14,6 +16,7 @@ class UserController extends Controller
     public function index()
     {
         //
+        return response()->json(['succcess' => true]);
     }
 
     /**
@@ -25,6 +28,26 @@ class UserController extends Controller
     public function store(Request $request)
     {
         //
+        $this->validate($request, [
+            "username"=> "required|string",
+            "password"=> "required|min:8",
+            "email" => "required|email:unique",
+        ]);
+        $hashedPassword = Hash::make($request->password);
+        $user = User::create([
+            "username"=> $request->username,
+            "password"=> $hashedPassword,
+            "email"=> $request->email,
+            "is_admin"=> $request->is_admin,
+            "is_staff"=> $request->is_staff,
+        ]);
+        $response = [
+            'success' => true,
+            'user' => $user,
+            'message' => 'User is registered',
+            'status' => 201,
+        ];
+        return response()->json( $response );
     }
 
     /**
