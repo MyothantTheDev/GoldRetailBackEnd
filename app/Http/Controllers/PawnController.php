0dde30501bb5fd034599845fa6ff1d752cce3a74
Myo Thant <jamesrobinson636478@gmail.com>
2024-01-02 01:08:57 +0700

    Bulit Skeleton for Sale and Pawn Controller

--- app/Http/Controllers/PawnController.php
+++ app/Http/Controllers/PawnController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pawn;
 use Illuminate\Http\Request;
-use QuickBooks_WebConnector_Server;
 
-class SaleController extends Controller
+class PawnController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -31,10 +31,10 @@ class SaleController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Pawn  $pawn
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Pawn $pawn)
     {
         //
     }
@@ -43,10 +43,10 @@ class SaleController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Models\Pawn  $pawn
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Pawn $pawn)
     {
         //
     }
@@ -54,10 +54,10 @@ class SaleController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\Pawn  $pawn
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Pawn $pawn)
     {
         //
     }
