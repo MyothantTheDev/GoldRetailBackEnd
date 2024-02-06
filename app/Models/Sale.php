ca16ab3e881ad5b0abc496168878e0dfddb39920
Myo Thant <003.myothant7729@gmail.com>
2024-02-07 01:04:36 +0700

    add many to many relationship in sale model

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 // use Illuminate\Database\Eloquent\Model;
-use Jenssegers\Mongodb\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
 
 class Sale extends Model
 {
@@ -13,7 +14,22 @@ class Sale extends Model
     protected $connection = "mongodb";
     protected $collection = "sales";
 
-    protected $fillable = ['price','gold','fee','gem','polish','total','discount','net',
+    protected $fillable = ['buyer','price','gold','fee','gem','polish','total','discount','net',
     'weight','encount','gem_weight'
     ];
+
+    public function totalweight(): BelongsTo
+    {
+        return $this->belongsTo(Weight::class, 'weight');
+    }
+
+    public function encounts(): BelongsTo
+    {
+        return $this->belongsTo(Weight::class, 'encount');
+    }
+
+    public function gemWeight(): BelongsTo
+    {
+        return $this->belongsTo(Weight::class, 'gem_weight');
+    }
 }
